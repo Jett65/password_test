31abfd3593b5b6fb7a2166d42ac702af278e6c94
Jett <jettc505@gmail.com>
2023-05-17 09:20:57 -0600

    got hasing working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-// Change to string later
-fn hash(code: &str) {
+fn hash(code: &str) -> String {
     let mut pass_list: Vec<u32> = Vec::new();
     let mut pre_list: Vec<u32> = Vec::new();
 
     for i in code.chars() {
         pass_list.push(i.to_digit(10).unwrap());
     }
+
     for i in 0..4 {
         pre_list.push(pass_list[0]);
         pass_list.remove(0);
     }
 
-    // hash the passcode
+    // hashes the passcode
     for i in 0..4 {
         pass_list.push((pass_list[0] * 3) + 4);
         pass_list.remove(0);
@@ -20,46 +20,34 @@ fn hash(code: &str) {
     for i in 0..4 {
         pre_list.push(pass_list[i])
     }
-}
-fn check(pass: &u32, compar_pass: &u32) -> bool {
-    // TODO: find out if there is a 0 in the first 4 numbers
-    // let mut char_list = Vec::new();
-    // for i in code.chars() {
-    //     char_list.push(i.to_digit(10).unwrap())
-    // }
-    //
-    // let mut i = 0;
-    // while i < 3 {
-    //     if char_list[i] == 0 {
-    //         println!("{}", true);
-    //         break;
-    //     }
-    //     i += 1;
-    //     if i == 3 {
-    //         println!("Invaled password")
-    //     }
-    // }
 
-    if pass == compar_pass {
-        return true;
-        // TODO: check if the last four numbers hashed are the same
-    } else {
-        return false;
+    let incode: String = pre_list
+        .into_iter()
+        .map(|i| i.to_string())
+        .collect::<String>();
+
+    println!("{incode}");
+    incode
+}
+
+fn check(pass: String, compar_pass: String) {
+    let mut compar_list: Vec<u32> = Vec::new();
+    for i in compar_pass.chars() {
+        compar_list.push(i.to_digit(10).unwrap());
+    }
+
+    for i in 0..4 {
+        if compar_list[i] == 0 {
+            println!("{:?}", true);
+        } else {
+            println!("{:?}", false)
+        }
     }
 }
 
 fn main() {
     let mut passcode = "01231234";
-    //let mut user_entry = 1234;
+    let mut user_enter = "10231234";
 
-    // let hentry = hash(&mut user_entry);
-    let hpass = hash(&passcode);
-
-    // if check(&hpass, &hentry) {
-    //     println!("Accuses Granted");
-    //     println!("Here is the super secret data:");
-    //     println!("I am the super secret data");
-    // } else {
-    //     println!("Accuses denide");
-    // }
+    check(hash(passcode), hash(user_enter));
 }
